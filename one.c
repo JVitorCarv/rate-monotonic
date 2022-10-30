29b92b7b3c398284de881020419a5b1fc6b5010c
Vitor <vitor.velozo2003@gmail.com>
2022-10-30 16:02:18 -0300

    Change logic from strtok to fscanf

--- one.c
+++ one.c
@@ -117,44 +117,26 @@ int main(int argc, char**argv) {
     file = fopen(argv[1], "r");
 
     Task* found_tasks = (Task*)malloc(total_tasks*sizeof(Task*));
-
+    
     int line_count = 0;   
     
-    while(fgets(line, MAX_LINE, file)){
-        if (line_count == 0) {
-            total_exe_time = atoi(line);
-        } else {
-            /*if (line[strlen(line)-1] == '\n') {
-                line[strlen(line)-1] = '\0';
-            }*/
-            int c = 0;
-            char* token = strtok(line, " ");
-            while (token != NULL && c < 3) {
-                printf("c == %d\n", c);
-                if (c == 0) {
-                    strcpy(found_tasks[line_count-1].task_name, token);
-                } else if (c == 1) {
-                    found_tasks[line_count-1].period = atoi(token);
-                    found_tasks[line_count-1].original_period = found_tasks[line_count-1].period;
-                    printf("Period: %d = %d\n", found_tasks[line_count-1].period, atoi(token));
-                } else if (c == 2) {
-                    found_tasks[line_count-1].time_unit = atoi(token);
-                    found_tasks[line_count-1].original_time_unit = found_tasks[line_count-1].time_unit;
-                    printf("TU: %d = %d\n", found_tasks[line_count-1].time_unit, atoi(token));
-                }
-                found_tasks[line_count-1].completed_count = 0;
-                found_tasks[line_count-1].lost_count = 0;
-                c++;
-                token = strtok(NULL, " ");
-                printf("Token: %s\n", token);
-            }
-        }
-        line_count++;
+    fgets(line, MAX_LINE, file);
+    total_exe_time = atoi(line); /* Somar 1 e depois subtrair */
+    
+    for (int i = 0; i < total_tasks; i++) {
+        fscanf(file, "%s%d%d", found_tasks[i].task_name, &found_tasks[i].period, &found_tasks[i].time_unit);
+        found_tasks[i].original_period = found_tasks[i].period;
+        found_tasks[i].original_time_unit = found_tasks[i].time_unit;
+        found_tasks[i].completed_count = 0;
+        found_tasks[i].lost_count = 0;
     }
+    print_task_array(found_tasks, total_tasks);
+
     fclose(file);
     printf("%d", line_count);
     Task ordered_tasks[total_tasks];
     printf("%d", total_tasks);
+
     // Copy found_tasks to ordered_tasks
     for (int i = 0; i < total_tasks; i++) {
         ordered_tasks[i] = found_tasks[i];
